b0b223dd6a561cf8860bd50bb73eeb92295e90a5
fungame2270 <tomasvictal@gmail.com>
2025-03-26 14:31:27 +0000

    added vite project

---
+++ src/f1-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <div className='bg-red-500'>
+      woof
+    </div>
+  )
+}
+
+export default App
