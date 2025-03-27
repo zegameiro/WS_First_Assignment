c4555fe730939cf9f5434816f076fcb502d7cd52
fungame2270 <tomasvictal@gmail.com>
2025-03-27 23:06:24 +0000

    added hot-reload added routes and router

--- src/f1-frontend/src/main.jsx
+++ src/f1-frontend/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {RouterProvider} from "react-router";
+import routes from './routes.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </StrictMode>,
 )
---
+++ src/f1-frontend/src/routes.jsx
@@ -0,0 +1,10 @@
+import {createBrowserRouter} from "react-router";
+
+const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello World</div>,
+    },
+  ]);
+
+export default routes;
