4bb3dd2decaf9a6c525815d3051a44bf7ef248d0
zegameiro <jose.mcgameiro@ua.pt>
2025-03-29 23:36:01 +0000

    Add constructor repository functions and refactor service methods to use them

--- src/f1-backend/app/services/constructors.py
+++ src/f1-backend/app/services/constructors.py
@@ -1,22 +1,11 @@
+from app.repositories.constructor import *
+
 import json
-from f1_pitstop.graph_db import db
-from app.constants import PRED, TYPE, NS
 
 def get_all_constructors():
     """Get all constructors"""
 
-    query = f"""
-        PREFIX pred: <{PRED}>
-        PREFIX type: <{TYPE}>
-        SELECT ?c ?name ?nationality ?url WHERE {{
-            ?c a type:Constructor ;
-            pred:name ?name ;
-            pred:nationality ?nationality ;
-            pred:url ?url .
-        }}
-    """
-
-    res = db.query(query)
+    res = retrieve_all_constructors()
     data = json.loads(res)
 
     results = []
@@ -33,18 +22,7 @@ def get_all_constructors():
 def get_constructors_by_nationality():
     """Get all the constructors grouped by nationality"""
 
-    query = f"""
-        PREFIX pred: <{PRED}>
-        PREFIX type: <{TYPE}>
-        SELECT ?nationality (GROUP_CONCAT(?name; separator=", ") AS ?constructors) WHERE {{
-            ?c a type:Constructor ;
-            pred:name ?name ;
-            pred:nationality ?nationality .
-        }}
-        GROUP BY ?nationality
-    """
-
-    res = db.query(query)
+    res = retrieve_constructors_by_nationality()
     data = json.loads(res)
 
     results = []
@@ -60,18 +38,7 @@ def get_constructors_by_nationality():
 def get_constructors_by_id(constructor_id):
     """Get a constructor from by it's id"""
 
-    query = f"""
-        PREFIX pred: <{PRED}>
-        PREFIX type: <{TYPE}>
-        SELECT ?name ?nationality ?url WHERE {{
-            <{constructor_id}> a type:Constructor ;
-            pred:name ?name ;
-            pred:nationality ?nationality ;
-            pred:url ?url .
-        }}
-    """
-
-    res = db.query(query)
+    res = retrieve_constructors_by_id(constructor_id)
     data = json.loads(res)
 
     binding = data['results']['bindings'][0]
