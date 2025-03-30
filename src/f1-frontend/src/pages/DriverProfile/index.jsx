b34658a0bd30aa3e08d07d56431ec31eb3baeb25
zegameiro <jose.mcgameiro@ua.pt>
2025-03-30 18:28:53 +0100

    Update DriverProfile component to enhance data display and add URL link

--- src/f1-frontend/src/pages/DriverProfile/index.jsx
+++ src/f1-frontend/src/pages/DriverProfile/index.jsx
@@ -26,15 +26,16 @@ const DriverProfile = () => {
           </h1>
 					<div className="flex gap-10 items-center border-2 border-white shadow-xl p-6 rounded-2xl">
 						<div className="flex flex-col text-xl space-y-3">
-							<p className="flex gap-4"><h1 className="font-semibold">First Name</h1> {driverData.data.data?.forename}</p>
-							<p className="flex gap-4"><h1 className="font-semibold">Last Name</h1> {driverData.data.data?.surname}</p>
-							<p className="flex gap-4"><h1 className="font-semibold">Date of Birth</h1> {driverData.data.data?.dob}</p>
+							<p className="flex gap-4"><span className="font-semibold">First Name</span> {driverData.data.data?.forename}</p>
+							<p className="flex gap-4"><span className="font-semibold">Last Name</span> {driverData.data.data?.surname}</p>
+							<p className="flex gap-4"><span className="font-semibold">Date of Birth</span> {driverData.data.data?.dob}</p>
 							{driverData.data.data?.code && (
 								<p className="flex gap-4"><h1 className="font-semibold">Code</h1> {driverData.data.data?.code}</p>
 							)}
 							{driverData.data.data?.number && (
 								<p className="flex gap-4"><h1 className="font-semibold">Number</h1> {driverData.data.data?.number}</p>
 							)}
+							<a className="badge badge-soft badge-primary text-lg" target="_blank" href={driverData.data.data?.url}>{driverData.data.data?.url}</a>
 						</div>
 						<div className="divider divider-horizontal"></div>
 						<div className="card bg-base-100 border-2 border-error shadow-sm w-max p-1">
